fcd550d0dfb1ae0bc9935836741731b32b70cb00
HurasAdam <huradam@wp.pl>
2024-06-08 21:13:19 +0200

    feat: Enhanced Service model schema with new fields and calculateFinalPrice method.

--- src/models/Service.ts
+++ src/models/Service.ts
@@ -1,10 +1,29 @@
-import { Schema, model } from "mongoose";
+import { Schema, model,Document } from "mongoose";
 
-const ServiceSchema = new Schema({
+export interface IService extends Document {
+    name: string;
+    description: string;
+    created: Date;
+    price: number;
+    promotionPrice?: number;
+    discountPrice?: number;
+    isPromotion: boolean;
+    estimatedTime: number;
+    animal: string;
+    image?: string;
+    reservationCount: number;
+    calculateFinalPrice(): Promise<number>;
+}
+
+
+const ServiceSchema = new Schema<IService>({
     name:{type:String,required:true,unique:true},
     description:{type:String,required:true},
     created:{type:Date,default:Date.now()},
     price:{type:Number,requied:true,},
+    promotionPrice:{type:Number,},
+    discountPrice:{type:Number},
+    isPromotion:{type:Boolean,default:false},
     estimatedTime:{type:Number,required:true},
     animal:{type:String,required:true},
     image:{type:String},
@@ -12,6 +31,9 @@ const ServiceSchema = new Schema({
     
 })
 
+ServiceSchema.methods.calculateFinalPrice =async function() {
+    return await this.isPromotion ? this.promotionPrice : this.price;
+};
 
 const Service = model("Service",ServiceSchema);
 export default Service;
