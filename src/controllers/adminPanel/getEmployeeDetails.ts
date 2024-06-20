324726af24fecca4fc3d613906e8a509bd9d9ab9
HurasAdam <huradam@wp.pl>
2024-06-20 20:46:05 +0200

    Fix: Corrected employee details retrieval by using the correct Employee model

--- src/controllers/adminPanel/getEmployeeDetails.ts
+++ src/controllers/adminPanel/getEmployeeDetails.ts
@@ -1,11 +1,11 @@
 import { Request,Response } from "express"
-import User from "../../models/User";
+import Employee from "../../models/Employee";
 
 export const getEmployeeDetails = async(req:Request,res:Response)=>{
 try{
 
     const { id } = req.params;
-const  employee = await User.findOne({_id:id}).select(["-password"])
+const  employee = await Employee.findById(id).select("-password")
 
 
 res.status(200).json(employee);
